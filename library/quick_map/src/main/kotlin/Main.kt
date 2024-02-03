cf3e75a9333a758cc12b60b1563f22b129215f71
Kevin Le Perf <codlabtech@gmail.com>
2024-02-03 10:18:31 +0100

    Remove unused logic for now

--- library/quick_map/src/main/kotlin/Main.kt
+++ library/quick_map/src/main/kotlin/Main.kt
@@ -1,46 +1,9 @@
-import eu.codlab.files.VirtualFile
-import eu.codlab.lorcana.Abilities
-import eu.codlab.lorcana.Franchises
-import eu.codlab.lorcana.GenericCard
-import eu.codlab.lorcana.Placeholders
-import eu.codlab.lorcana.Set
-import eu.codlab.lorcana.utils.Provider
 import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.builtins.ListSerializer
-import kotlinx.serialization.builtins.serializer
 
-/**
- * Prepare the possible future of the predefined data
- *
- * yaml -> serializer -> library -> json
- *                               -> protobuf
- *                               -> yaml
- *                               -> etc...
- */
 fun main() {
     runBlocking {
-        val file = VirtualFile.Root
-        val yamls = VirtualFile(file, "../../data")
-        yamls.mkdirs()
-
-        listOf(
-            "abilities_yaml.yml" to Abilities.loadFromResource(),
-            "franchises_yaml.yml" to Franchises.loadFromResource(),
-            "placeholders_yaml.yml" to Placeholders.loadFromResource()
-        ).forEach { (file, content) ->
-            val vfile = VirtualFile(yamls, file)
-            vfile.write(Provider.yaml.encodeToString(content).toByteArray())
-        }
-
-        val serializer =
-            ListSerializer(GenericCard.serializer(String.serializer(), String.serializer()))
-        listOf(
-            Triple("d23_yaml.yml", Set.D23.loadFromResource(), serializer),
-            Triple("rotf_yaml.yml", Set.ROTF.loadFromResource(), serializer),
-            Triple("tfc_yaml.yml", Set.TFC.loadFromResource(), serializer)
-        ).forEach { (file, content, serializer) ->
-            val vfile = VirtualFile(yamls, file)
-            vfile.write(Provider.yaml.encodeToString(serializer, content).toByteArray())
-        }
+        // note : removed the logic, this is not ready yet for complete manipulation
+        // and will be renamed so that it highlights the future capability of generating the
+        // various assets which could be production ready in each releases :)¬
     }
 }
