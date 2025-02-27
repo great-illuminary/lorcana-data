2bc9b604d80cd44e6a379d7c42b43fed9ef74cc0
Kévin Le Perf <codlabtech@gmail.com>
2025-02-27 18:11:17 +0100

    update yarn lock

--- scripts/check.sh
+++ scripts/check.sh
@@ -3,7 +3,7 @@
 set -xe
 
 # generation, to be sure about the current state
-./gradlew generateBuildKonfig --no-daemon
+./gradlew kotlinUpgradeYarnLock generateBuildKonfig --no-daemon
 
 # running validation --no-daemon
 ./gradlew ktlint detekt --no-daemon
